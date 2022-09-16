1a77e3538c247eb6b6a228ed178a5fb46b08049b
Evgeniy <xelseo@gmail.com>
2022-09-16 14:41:31 +0300

    Created a test

---
+++ src/test/java/com/demoqa/TestStudentRegistrationForm.java
@@ -0,0 +1,31 @@
+package com.demoqa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestStudentRegistrationForm {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void fillFormTest() {
+        String firstName = "Egor";
+        String lastName = "Petrov";
+        String email = "Petrov@egor.com";
+        String subjects = "coding";
+        String adress = "Kremlin";
+
+        open("/automation-practice-form");
+    }
+
+}
